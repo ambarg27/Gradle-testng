9992a6f0b52b1df6a0e92898be7ec03dcc577da8
ambarg27 <146844240+ambarg27@users.noreply.github.com>
2025-12-24 13:07:07 +0530

    Update Sample2.java

--- src/test/java/com/test/app/Sample2.java
+++ src/test/java/com/test/app/Sample2.java
@@ -1,5 +1,5 @@
 /*
-@author Kunal Soni
+@author Kunal
 */
 
 package com.test.app;
@@ -12,11 +12,13 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 
 public class Sample2 extends TestBaseClass {
 
     String status;
+
     @Test(groups = "Smoke")
     public void tc00VerifyURL() throws InterruptedException {
 
@@ -24,65 +26,66 @@ public class Sample2 extends TestBaseClass {
                 .assignCategory("Functional_TestCase")
                 .assignCategory("Positive_TestCase")
                 .assignAuthor("TestUser");
+
         logger.info("Verify URL");
+
         webdriver.openURL("https://www.jivrus.com/resources/articles/technical/how-to-open-browser-console-log");
         webdriver.openURL("https://lambdatest.github.io/sample-todo-app/");
+
         test.log(Status.INFO, "Open URL");
         logger.info("Open URL");
+
         Thread.sleep(5000);
     }
 
     @Test
     public void tc01VerifyEnterText() {
-try {
-    
+        try {
+            test = extent.createTest("Sample Test 2", "")
+                    .assignCategory("Functional_TestCase")
+                    .assignCategory("Positive_TestCase")
+                    .assignAuthor("Kunal");
 
-        test = extent.createTest("Sample Test 2", "")
-                .assignCategory("Functional_TestCase")
-                .assignCategory("Positive_TestCase")
-                .assignAuthor("Kunal");
+            test.log(Status.PASS, "URL is opened");
 
-        test.log(Status.PASS, "URL is opened");
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        test.log(Status.PASS, "Wait created");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            test.log(Status.PASS, "Wait created");
 
-        By textField = By.id("sampletodotext");
+            By textField = By.id("sampletodotext");
+            WebElement addText = driver.findElement(textField);
 
-        WebElement addText = driver.findElement(textField);
+            int item_count = 5;
 
-        int item_count = 5;
-
-        for (int i = 1; i <= item_count; i++) {
-            addText.click();
-            addText.sendKeys("Adding a new item " + i + Keys.ENTER);
-            test.log(Status.PASS, "New item No. " + i + " is added");
-        }
-
-        WebElement temp_element;
-
-        int totalCount = item_count+5;
-        int remaining = totalCount-1;
-
-        for (int i = 1; i <= totalCount; i++, remaining--) {
-
-            String xpath = "(//input[@type='checkbox'])["+i+"]";
-
-            driver.findElement(By.xpath(xpath)).click();
-            test.log(Status.PASS, "Item No. " + i + " marked completed");
-            By remainingItem = By.className("ng-binding");
-            String actualText = driver.findElement(remainingItem).getText();
-            String expectedText = remaining+" of "+totalCount+" remaining";
-
-            if (!expectedText.equals(actualText)) {
-                test.log(Status.FAIL, "Wrong Text Description");
-                status = "failed";
+            for (int i = 1; i <= item_count; i++) {
+                addText.click();
+                addText.sendKeys("Adding a new item " + i + Keys.ENTER);
+                test.log(Status.PASS, "New item No. " + i + " is added");
             }
 
-            test.log(Status.PASS, "Item No. " + i + " completed");
+            int totalCount = item_count + 5;
+            int remaining = totalCount - 1;
+
+            for (int i = 1; i <= totalCount; i++, remaining--) {
+                String xpath = "(//input[@type='checkbox'])[" + i + "]";
+                driver.findElement(By.xpath(xpath)).click();
+
+                test.log(Status.PASS, "Item No. " + i + " marked completed");
+
+                By remainingItem = By.className("ng-binding");
+                String actualText = driver.findElement(remainingItem).getText();
+                String expectedText = remaining + " of " + totalCount + " remaining";
+
+                if (!expectedText.equals(actualText)) {
+                    test.log(Status.FAIL, "Wrong Text Description");
+                    status = "failed";
+                }
+
+                test.log(Status.PASS, "Item No. " + i + " completed");
+            }
+
+        } catch (Exception e) {
+            test.log(Status.FAIL, "Exception Occurred: " + e.getMessage());
+            status = "failed";
         }
-    } catch (Exception e) {
-        // TODO: handle exception
-    }
     }
 }
-
